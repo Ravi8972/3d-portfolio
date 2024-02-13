ec70bb9f1925ab6cff10b0d8eabd0e03b9465296
Raviranjan Mishra <ravimishra.rm9558@gmail.com>
2024-02-13 06:04:30 +0530

    first

---
+++ src/assets/images/index.js
@@ -0,0 +1,17 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import tataelxsi_logo from  './tataelxsi_logo.jpg'
+import logo from './logo.svg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    tataelxsi_logo,
+    logo
+}
